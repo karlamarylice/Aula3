f3c42759e87400cdb1beb9223ac7826bd9fc788e
karlamarylice <karlamarylicem@gmail.com>
2023-01-24 11:22:09 -0300

    Meus Livros Favoritos

---
+++ livrosFavoritos/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("========= MEUS LIVROS FAVORITOS =============\n" +
+                "1 - As Estrelas e Outras Drogas\n" +
+                "2 - After Fire\n" +
+                "3 - Beijos de Mentira\n");
+    }
+}
